9e9d7e7e821eb82667149bd8da01161f9678b1b6
Andrey Piskunov <andrey.piskunov@corp.mail.ru>
2024-12-03 19:54:12 +0900

    Initial commit

---
+++ npx/components/CalculateButton.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function CalculateButton(props) {
+  return (
+    <>
+      <button>+</button>
+      <button>-</button>
+      <br />
+      <button>*</button>
+      <button>/</button>
+    </>
+  );
+}
+
+export default CalculateButton;
