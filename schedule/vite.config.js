d4bd410e0b5ea42e2160bd31c168cb71f33c339f
Diatlova-Nataliia <natasha200124@gmail.com>
2025-01-16 16:31:24 +0900

    fixed docs build script to account for public deploy url /playground/

--- schedule/vite.config.js
+++ schedule/vite.config.js
@@ -3,6 +3,5 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  // base: "/playground/schedule/dist/", // for local dev
   plugins: [react()],
 });
